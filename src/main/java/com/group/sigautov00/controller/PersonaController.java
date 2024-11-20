2ba83316dc14c3e00d21c6a842113a2bfe8a1a00
axvg <axvg@users.noreply.github.com>
2024-11-19 19:44:45 -0500

    add persona controller

--- src/main/java/com/group/sigautov00/controller/PersonaController.java
+++ src/main/java/com/group/sigautov00/controller/PersonaController.java
@@ -1,14 +1,56 @@
 package com.group.sigautov00.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.group.sigautov00.entity.Persona;
+import com.group.sigautov00.service.PersonaService;
+
 @RestController
-@RequestMapping("/persona")
+@RequestMapping("/api/personas")
 public class PersonaController {
-    @GetMapping("")
-    public String getPersona() {
-        return "{ \"name\": \"John\", \"age\": 1 }";
+
+    private static final Logger logger = LoggerFactory.getLogger(PersonaController.class);
+
+    @Autowired
+    private PersonaService personaService;
+
+    @GetMapping
+    public ResponseEntity<List<Persona>> getAll() {
+        try {
+            List<Persona> personas = personaService.getAll();
+            return new ResponseEntity<List<Persona>>(personas, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("Error fetching personas", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Persona> createPersona(@RequestBody Persona persona){
+        try {
+            Persona savedPersona = personaService.createPersona(persona);
+            return new ResponseEntity<>(savedPersona, HttpStatus.CREATED);
+        } catch (Exception e) {
+            logger.error("Error creating persona", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        logger.error("Unhandled exception", e);
+        return new ResponseEntity<>("An error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
