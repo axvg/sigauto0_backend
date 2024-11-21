a920e92ce63f1699dfa8ccbcd764c034d437fe6b
axvg <axvg@users.noreply.github.com>
2024-11-20 21:41:03 -0500

    fix cardinality

--- src/main/java/com/group/sigautov00/entity/Cliente.java
+++ src/main/java/com/group/sigautov00/entity/Cliente.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,7 +26,7 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCliente;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "persona_id", nullable = false)
     private Persona persona;
 
