3bb8e92e5c827ca31752f1a7a1064e2865b3dda7
axvg <azvegab@gmail.com>
2024-12-05 15:16:05 -0500

    update

--- src/main/java/com/group/sigautov00/repository/ClienteRepository.java
+++ src/main/java/com/group/sigautov00/repository/ClienteRepository.java
@@ -1,7 +1,10 @@
 package com.group.sigautov00.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 import com.group.sigautov00.entity.Cliente;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
--- src/main/java/com/group/sigautov00/repository/PersonaRepository.java
+++ src/main/java/com/group/sigautov00/repository/PersonaRepository.java
@@ -1,9 +1,11 @@
 package com.group.sigautov00.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.group.sigautov00.entity.Persona;
 
+@Repository
 public interface PersonaRepository extends JpaRepository<Persona, Long>  {
     boolean existsByEmail(String email);
     boolean existsByNumeroDocumento(Long numeroDocumento);
